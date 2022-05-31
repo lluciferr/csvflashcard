fe24cd6cbde8438970346bfd9be49570d56f0b00
lluciferr <82355776+lluciferr@users.noreply.github.com>
2022-05-31 17:59:36 +0545

    improved  cache.addAll

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener("install",(e)=>{
     console.log("install !!!")
     e.waitUntil(
         caches.open("static").then(cache =>{
-            return cache.addAll(["./","./assets/icon192.png"])
+            return cache.addAll(["./"])
         })
 )
 
